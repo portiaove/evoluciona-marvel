3b7aaab5495b75e41d7285c9da5555abe52fe249
Juan Portillo <juanportillog@gmail.com>
2019-10-14 13:10:09 +0200

    show more/show less button

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
-const Characters = (props) => {
+class Characters extends React.Component {
 
-  const { name, thumbnail } = props.character
+  state = {
+    readMore: false
+  }
 
-  const description = props.character.description.replace(/<p class="Body">/g, '').replace(/<\/p>/g, '')
+  handleReadMore = () => {
+    const { readMore } = this.state
+    this.setState({readMore: !readMore})
+  }
 
+  render() {
+  const { name, thumbnail } = this.props.character
   const image = `${thumbnail.path}.${thumbnail.extension}`
+
+  const descriptionLong = this.props.character.description.replace(/<p class="Body">/g, '').replace(/<\/p>/g, '')
+  const descriptionShort = `${descriptionLong.slice(0, 100)}...`
+
+  const description = this.state.readMore ? descriptionLong : descriptionShort
+  
+  const text = description === '...' || description === ' ...'? 'No description available at the moment :(' : description
+  const readText = this.state.readMore ? 'Show Less' : 'Show More'
 
   return (
     <div className='Characters'>
@@ -15,10 +30,12 @@ const Characters = (props) => {
       </div>
       <div className ='Characters-Info'>
         <h2>{name}</h2>
-        <p>{description ? description : 'No description available at the moment :('}</p>
+        <p>{text}</p>
+        {text !== 'No description available at the moment :(' && <button className='Btn-ReadMore' onClick={this.handleReadMore}>{readText}</button>}
       </div>
     </div>
   )
+  }
 }
 
 export default Characters
